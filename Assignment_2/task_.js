577b4ebb1a843e71e982ad3383235ee1ca06f4cd
Muhammad Bin Salman <muhammad.binyammani@gmail.com>
2022-11-01 11:31:19 -0700

    10 Tasks Completed
    Till now I've completed 10 task of javascript

---
+++ Assignment_2/task_.js
@@ -0,0 +1,27 @@
+//Task 6 starts
+//Muhammad Bin Salman                         
+//Date: 11/1/2022 (Task 9 part)
+console.log(6+2);            //This program will add 6 in 2 and print the result
+console.log(20-12);          //This program will subtract 12 from 20 and print the result
+console.log(2*4);            //This program will multiply 2 with 4 and print the result
+console.log(16/2);           //Thie program will divide 16 by 2 and print the answer
+
+//Task 7 starts
+console.log(1+7);
+console.log(3+5);
+console.log(4+4);
+console.log(6+2);
+
+//Task 8 starts
+//Muhammad Bin Salman                          
+//Date: 11/1/2022
+let fav_num=3;                                      //This program will print my favourite number which
+console.log(`My favourite number is ${fav_num}`);   //is already stored in a variable named fav_num by
+                                                    //just calling it
+//Task 9 
+
+//Task 10
+let friends= ["Abdullah", "Shehzad", "Taha"];
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
